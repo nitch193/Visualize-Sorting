1dd788f994754b9417b44d6e169205c39ec9c10e
nitch193 <niteshchaurasiya193@gmail.com>
2021-03-16 12:26:08 +0530

    added webpack :package: and :wrench: updated config

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
+};
